14e6922a66633ad79c11057e7fe59f54db4ebaa4
Jennifer <jenniferpeterson@pursuit.org>
2024-05-16 22:16:20 -0400

    rendering the data from the past productions onto the page with the help from uuid package

--- src/Components/ListView.jsx
+++ src/Components/ListView.jsx
@@ -1,8 +1,24 @@
+import { v4 as uuidv4 } from "uuid";
 
-const ListView = () => {
+const ListView = ({
+  activeProductions,
+  setActiveProductions,
+  pastProductions,
+  setPastProductions,
+}) => {
+  console.log(activeProductions);
   return (
-    <div style={{ background: "palegoldenrod" }}>ListView</div>
-  )
-}
+    <div>
+      <div style={{ background: "palegoldenrod" }}>ListView</div>
+      <ul>
+        {activeProductions.map(({ event_name }) => (
+          <li key={uuidv4()}>{event_name}</li>
+        ))}
+      </ul>
+      {/* button for the past filiming locations/ productions */}
+      <button>See Past Filming Locations</button>
+    </div>
+  );
+};
 
-export default ListView
+export default ListView;
