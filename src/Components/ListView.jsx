3d6405fea2f5002bef9f50a186b5adea25d6ebb0
AnitaOwen <anitaowen@pursuit.org>
2024-05-17 15:37:37 -0400

    added dropdown to filter by borough

--- src/Components/ListView.jsx
+++ src/Components/ListView.jsx
@@ -9,6 +9,16 @@ const ListView = ({
   setPastProductions,
 }) => {
   const [active, setActive] = useState(true);
+  const [selectedBorough, setSelectedBorough] = useState("")
+
+  const handleBoroughChange = (event) => {
+    setSelectedBorough(event.target.value);
+  }
+  const filteredActiveProductions = selectedBorough ? activeProductions.filter(({ event_borough }) => event_borough === selectedBorough) : activeProductions
+
+  const filteredPastProductions = selectedBorough ? pastProductions.filter(({ borough }) => borough === selectedBorough) : pastProductions
+
+  
 
   function formatDateToHumanReadable(dateString) {
     const date = new Date(dateString);
@@ -17,9 +27,22 @@ const ListView = ({
     const year = date.getFullYear();
     return `${month}/${day}/${year}`;
   }
+
   return (
     <div>
       <button onClick={() => setActive(!active)}>See {active ? "Past Filming Locations" : "Recent and Ongoing Production Events"}</button>
+
+      <div>
+        <label htmlFor="borough-select">Filter by Borough: </label>
+        <select id="borough-select" value={selectedBorough} onChange={handleBoroughChange}>
+          <option value="">All Boroughs</option>
+          <option value="Manhattan">Manhattan</option>
+          <option value="Brooklyn">Brooklyn</option>
+          <option value="Queens">Queens</option>
+          <option value="Bronx">Bronx</option>
+          <option value="Staten Island">Staten Island</option>
+        </select>
+      </div>
 
       <div style={{ background: "yellow" }}>
         {active && (
@@ -27,7 +50,7 @@ const ListView = ({
           <h3>Recent and Ongoing Production Events</h3>
           <Link to="/map" style={{ color: "red" }}>View Map</Link>
           <ul>
-            {activeProductions.map(({event_name, event_borough, event_location, street_closure_type, start_date_time, end_date_time }) => (
+            {filteredActiveProductions.map(({event_name, event_borough, event_location, street_closure_type, start_date_time, end_date_time }) => (
               <li key={uuidv4()} style={{ marginBottom: '10px' }}>
                 <div>
                   <h4>({event_borough}) {event_name}</h4>
@@ -50,7 +73,7 @@ const ListView = ({
           <h3>Past Filming Locations</h3>
           <Link to="/map" style={{ color: "red" }}>View Map</Link>
           <ul>
-          {pastProductions.map(({ subcategoryname, eventid, category, borough, startdatetime, enddatetime, parkingheld }) => (
+          {filteredPastProductions.map(({ subcategoryname, eventid, category, borough, startdatetime, enddatetime, parkingheld }) => (
             <li key={eventid} style={{ marginBottom: '10px' }}>
               <div>
                 <h4>
