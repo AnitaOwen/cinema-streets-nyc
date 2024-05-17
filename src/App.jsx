45a59695596757fce893ebfed81cca5f146290a7
Jennifer <jenniferpeterson@pursuit.org>
2024-05-16 20:26:11 -0400

    added a Map Component and the route

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<LandingPage />}/>
-      <Route path="/map" element={<MapView />}/>  
-      <Route path="/list" element={<ListView />}/>      
-      <Route path="/about" element={<About />}/> 
-    </Routes>
-    <Footer />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/map" element={<MapView />} />
+        <Route path="/list" element={<ListView />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 };
 
 export default App;
