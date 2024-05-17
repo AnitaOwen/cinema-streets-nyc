7ee0a96e153e867774d55ffa5fe64f18e80066a8
Jennifer <jenniferpeterson@pursuit.org>
2024-05-16 22:15:41 -0400

    made a fecth call to get the active productions and passed it to list view as a prop

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,58 @@ import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const API = import.meta.env.VITE_ACTIVE_URL;
+  const PAST_API = import.meta.env.VITE_PAST_PRODUCTIONS_URL;
+
+  const [activeProductions, setActiveProductions] = useState([]);
+  const [pastProductions, setPastProductions] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}`).then((res) => {
+      res.json().then((data) => setActiveProductions(data));
+    });
+  }, []);
+
+  // const fetchPastProductions = async () => {
+  //   try {
+  //     let allPastProductions = [];
+  //     for (let i = 0; i < 5; i++) {
+  //       const data = await fetch(
+  //         `${PAST_API}?$offset=${i * 1000}&$order=:eventid`
+  //       ).then((res) => res.json());
+  //       console.log(data);
+  //       allPastProductions = allPastProductions.concat(data);
+  //       console.log(allPastProductions);
+  //     }
+  //     // setPastProductions(allPastProductions);
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   fetchPastProductions();
+  // }, []);
+
   return (
     <>
       <NavBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/map" element={<MapView />} />
-        <Route path="/list" element={<ListView />} />
+        <Route
+          path="/list"
+          element={
+            <ListView
+              activeProductions={activeProductions}
+              setActiveProductions={setActiveProductions}
+              setPastProductions={setPastProductions}
+              pastProductions={pastProductions}
+            />
+          }
+        />
         <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
